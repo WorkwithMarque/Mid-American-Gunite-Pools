f9f2107ccf9359625355cc24b64c06a20d5602f5
codemarque <codesultan369@gmail.com>
2025-03-27 17:58:30 +0100

    wip

--- seeds/seed_users.js
+++
@@ -1,8 +0,0 @@
-export function seed(knex) {
-  return knex('users').del() // Deletes existing data
-    .then(() => {
-      return knex('users').insert([
-        { email: 'test@example.com', workyard_user_id: '12345' }
-      ]);
-    });
-}
--- src/db.js
+++
@@ -1,8 +0,0 @@
-
-import knex from 'knex';
-import config from '../knexfile.js';
-
-const environment = process.env.NODE_ENV || 'development'; // Use 'development' by default
-const db = knex(config[environment]);
-
-export default db;
