6ab7189b89f2540adc23125b3c47686b73a6fa0e
codemarque <codesultan369@gmail.com>
2025-03-27 18:39:00 +0100

    wip

--- knexfile.js
+++ knexfile.js
@@ -21,17 +21,17 @@ export default {
     }
   },
 
-  // production: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   },
-  //   useNullAsDefault: true, // Required for SQLite
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //     directory: `${__dirname}/migrations`
-  //   }
-  // },
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true, // Required for SQLite
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/migrations`
+    }
+  },
 
   // staging: {
   //   client: 'postgresql',
@@ -47,20 +47,20 @@ export default {
   //   }
   // },
 
-  production: {
-    client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT || 3306
-    },
-    pool: { min: 2, max: 10 },
-    migrations: {
-      tableName: 'knex_migrations',
-      directory: `${__dirname}/migrations`
-    }
-  }
+  // production: {
+  //   client: 'mysql2',
+  //   connection: {
+  //     host: process.env.DB_HOST,
+  //     database: process.env.DB_NAME,
+  //     user: process.env.DB_USER,
+  //     password: process.env.DB_PASSWORD,
+  //     port: process.env.DB_PORT || 3306
+  //   },
+  //   pool: { min: 2, max: 10 },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //     directory: `${__dirname}/migrations`
+  //   }
+  // }
 
 };
