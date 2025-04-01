feb275dd7a30e0f594c71f34eb06dc01ddfb7d29
codemarque <codesultan369@gmail.com>
2025-04-01 13:34:17 +0100

    wip

--- migrations/20250401101150_create_projects_table.cjs
+++ migrations/20250401101150_create_projects_table.cjs
@@ -3,6 +3,7 @@ exports.up = function (knex) {
       table.increments("id").primary();
       table.string("workyard_project_id", 255).notNullable();
       table.string("jobthread_job_id", 255);
+      table.string("name", 255); 
     });
   };
   
