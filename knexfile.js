b7400bb3befcd1ab19f45ddbc4142318b64c615e
codemarque <codesultan369@gmail.com>
2025-04-06 10:44:50 +0100

    wip

--- knexfile.js
+++ knexfile.js
@@ -1,52 +1,91 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
-import config from './src/config';
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import dotenv from 'dotenv';
+// import config from './src/config';
+
+// // Load environment variables
+// // dotenv.config();
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// export default {
+//   development: {
+//     client: 'sqlite3',
+//     connection: {
+//       filename: './dev.sqlite3'
+//     },
+//     useNullAsDefault: true, // Required for SQLite
+//     migrations: {
+//       tableName: 'knex_migrations',
+//       directory: `${__dirname}/migrations`
+//     }
+//   },
+
+//   // production: {
+//   //   client: 'sqlite3',
+//   //   connection: {
+//   //     filename: './dev.sqlite3'
+//   //   },
+//   //   useNullAsDefault: true, // Required for SQLite
+//   //   migrations: {
+//   //     tableName: 'knex_migrations',
+//   //     directory: `${__dirname}/migrations`
+//   //   }
+//   // },
+
+//   // staging: {
+//   //   client: 'postgresql',
+//   //   connection: {
+//   //     database: 'american',
+//   //     user: 'user',
+//   //     password: 'password'
+//   //   },
+//   //   pool: { min: 2, max: 10 },
+//   //   migrations: {
+//   //     tableName: 'knex_migrations',
+//   //     directory: `${__dirname}/migrations`
+//   //   }
+//   // },
+
+//   production: {
+//     client: 'mysql2',
+//     connection: {
+//       host: config.mysql.host,
+//       database: config.mysql.database,
+//       user: config.mysql.user,
+//       password: config.mysql.password,
+//       port: config.mysql.port
+//     },
+//     pool: { min: 2, max: 10 },
+//     migrations: {
+//       tableName: 'knex_migrations',
+//       directory: `${__dirname}/migrations`
+//     }
+//   }
+
+// };
+const { dirname } = require('path');
+const { fileURLToPath } = require('url');
+const dotenv = require('dotenv');
+const config = require('./src/config');
 
 // Load environment variables
 // dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default {
+module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
     },
-    useNullAsDefault: true, // Required for SQLite
+    useNullAsDefault: true,
     migrations: {
       tableName: 'knex_migrations',
       directory: `${__dirname}/migrations`
     }
   },
-
-  // production: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './dev.sqlite3'
-  //   },
-  //   useNullAsDefault: true, // Required for SQLite
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //     directory: `${__dirname}/migrations`
-  //   }
-  // },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'american',
-  //     user: 'user',
-  //     password: 'password'
-  //   },
-  //   pool: { min: 2, max: 10 },
-  //   migrations: {
-  //     tableName: 'knex_migrations',
-  //     directory: `${__dirname}/migrations`
-  //   }
-  // },
-
   production: {
     client: 'mysql2',
     connection: {
@@ -62,5 +101,4 @@ export default {
       directory: `${__dirname}/migrations`
     }
   }
-
 };
